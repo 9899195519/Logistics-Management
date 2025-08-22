292645f029b5bdc0aa121a5f7bdfe9db7501d5f3
Dhiraj Kumar <dhiraj5_kumar@yahoo.co.in>
2025-08-22 21:38:04 +0530

    initial commit

---
+++ Logistics-Management/src/main/java/com/dhiraj/logistics/LogisticsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dhiraj.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsManagementApplication.class, args);
+	}
+
+}
